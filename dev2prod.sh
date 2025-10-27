191080c946c1f60c7c2912d2a288a9e42849b4a6
Max <max@maxf.net>
2025-10-27 13:02:23 +0000

    updated

--- dev2prod.sh
+++ dev2prod.sh
@@ -2,13 +2,17 @@
 
 SWD=$(dirname $0)
 
-devPodName=ws-pubsub-dev
-prodPodName=ws-pubsub-prod
-containerName=ws-pubsub
+name=$(basename $SWD)
+devPodName=$name-dev
+prodPodName=$name-prod
+containerName=$name
 
-devImage=$(yq 'select(.metadata.name == "'$devPodName'")  | .spec.template.spec.containers[] | select(.name == "'$containerName'") |  .image' $SWD/k8s/04-deployment-dev.yaml)
+devYaml=$(ls -1 $SWD/k8s/*-deployment-dev.yaml)
+prodYaml=$(ls -1 $SWD/k8s/*-deployment-prod.yaml)
 
-prodImage=$(yq 'select(.metadata.name == "'$prodPodName'")  | .spec.template.spec.containers[] | select(.name == "'$containerName'") |  .image' $SWD/k8s/05-deployment-prod.yaml)
+devImage=$(yq 'select(.metadata.name == "'$devPodName'")  | .spec.template.spec.containers[] | select(.name == "'$containerName'") |  .image' $devYaml)
+
+prodImage=$(yq 'select(.metadata.name == "'$prodPodName'")  | .spec.template.spec.containers[] | select(.name == "'$containerName'") |  .image' $prodYaml)
 
 docker tag $devImage $prodImage
 
@@ -19,4 +23,11 @@ while read -u 3 node_id; do
 done 3< <(kubectl get nodes -o json|jq -r '.items[].metadata.name')
 wait
 
+kubectl apply -f $prodYaml
 kubectl -n dpsrv rollout restart deployment $prodPodName
+
+version=$(yq '.spec.template.spec.containers[].image | capture(".*-(?<ver>[0-9]+\\.[0-9]+\\.[0-9]+)$").ver' $prodYaml)
+git commit -a -m "Released $version"
+git push
+yq -i '.spec.template.spec.containers[].image |= (capture("(?<pre>.*-)(?<maj>[0-9]+)\\.(?<min>[0-9]+)\\.(?<patch>[0-9]+)$") | "\(.pre)\(.maj).\(.min).\((.patch|tonumber + 1))")' $prodYaml
+
